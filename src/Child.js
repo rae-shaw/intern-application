530a2fd0268e1b859bd9fa79acacb680bfd6726e
Rachael Shaw <rachaelleeshaw@gmail.com>
2020-05-12 18:25:43 -0500

    blink counter added

--- src/Child.js
+++ src/Child.js
@@ -1,5 +1,28 @@
-function Child() {
-  return null
+import React, { useState, useEffect } from 'react';
+
+function Child(props) {
+	//console.log(props)
+	let blink = props.blink
+	
+
+	const [blinkCounts, setCount] = useState(0)
+
+	
+	useEffect(() => {
+
+    	blink ? setCount(blinkCounts => blinkCounts + 1) : setCount(blinkCounts => blinkCounts + 0)
+ 
+  	}, [blink]); 
+	console.log(blinkCounts)
+  	return(
+  		<>
+  			<header>
+  				<p>Doctor Who is the Best</p>
+				 
+  				{blink ? <h1>DON'T</h1> : <h1>BLINK</h1> }
+  			</header>
+  		</>
+  	)
 }
 
 export default Child
