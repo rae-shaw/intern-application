eb918ae2e06f1ab02a819d3057935fffccffa2aa
Rachael Shaw <rachaelleeshaw@gmail.com>
2020-05-12 19:12:46 -0500

    added angels

--- src/Child.js
+++ src/Child.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import Angel1 from './images/angel_1.jpg';
+import Angel2 from './images/angel_2.jpg';
+import Angel3 from './images/angel_3.jpg';
 
 function Child(props) {
 	//console.log(props)
@@ -14,15 +17,21 @@ function Child(props) {
  
   	}, [blink]); 
 	console.log(blinkCounts)
-  	return(
-  		<>
-  			<header>
-  				<p>Doctor Who is the Best</p>
-				 
-  				{blink ? <h1>DON'T</h1> : <h1>BLINK</h1> }
-  			</header>
-  		</>
-  	)
+		return(
+			<>
+				<header>
+					{blink ? <h1>DON'T</h1> : <h1>BLINK</h1> }
+				</header>
+				<main>
+					{blinkCounts < 5 ? <img src = {Angel1} /> 
+					: blinkCounts >= 5 && blinkCounts < 10 ? <img src = {Angel2} /> 
+					: <img src = {Angel3} /> }
+				</main>
+
+			</>
+
+		)
 }
+
 
 export default Child
