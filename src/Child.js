a61d4e0c73e3262e8096257e9d598b3ede777e8b
Rachael Shaw <rachaelleeshaw@gmail.com>
2020-05-21 08:35:16 -0500

    fixed linter issues

--- src/Child.js
+++ src/Child.js
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Angel1 from './images/angel_1.jpg';
 import Angel2 from './images/angel_2.jpg';
 import Angel3 from './images/angel_3.jpg';
 import dontBlink from './images/DONT-BLINK.png'
 import './Child.css'
 
-function Child(props) {
-	let blink = props.blink
+function Child({blink, color, setColor, setInterval}) {
 	
 	const [blinkCounts, setCount] = useState(0)
+
+	const prevBlink = useRef(blink)
 	
 	useEffect(() => {
+		setInterval(1000/(blinkCounts * .3))
 
-		const blinkInterval = props.setInterval(1000/(blinkCounts * .3))
+		const nextColor = blinkCounts < 3 ? '#000000'
+			: blinkCounts >= 3 && blinkCounts < 6 ? '#B22222'
+			: '#8B0000'
 
-			blinkCounts < 3 ? props.setColor('#000000')
-			: blinkCounts >= 3 && blinkCounts < 6 ? props.setColor('#B22222') 
-			: props.setColor('#8B0000')
+		setColor(nextColor)
 
-    	blink ? setCount(blinkCounts => blinkCounts + 1) : setCount(blinkCounts => blinkCounts + 0)
-  	}, [blink]); 
+		if (blink && !prevBlink.current) {
+			setCount(blinkCounts => blinkCounts + 1)
+		}
+		console.log("blinkCounts", blinkCounts)
+
+		prevBlink.current = blink
+	}, [blink, blinkCounts, color, setColor, setInterval]);
+
 
 	if(blinkCounts < 3){
 		return(
@@ -31,8 +39,8 @@ function Child(props) {
 	}else if (blinkCounts >= 3 && blinkCounts < 15){
 		return( 
 			<section className = 'fullSection'>
-				<header style = {{color: props.color}}>
-					{blink ? <h1 >DON'T</h1> : <h1>BLINK</h1> }
+				<header style = {{color: color}}>
+					{blink ? <h1>DON'T</h1> : <h1>BLINK</h1> }
 				</header>
 				<main>
 					{blinkCounts < 5 ? <img alt = 'A weeping angel with its head in hands.' className = 'angelOne' src = {Angel1} /> 
